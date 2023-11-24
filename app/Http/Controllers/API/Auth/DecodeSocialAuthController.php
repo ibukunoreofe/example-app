8a2fb7bf568fc8558790e5435be3be54c552b7d6
Ibukun Bello <ibukunoreofe@gmail.com>
2023-11-24 16:24:25 +0100

    Added Google Client IdToken Decoder

--- app/Http/Controllers/API/Auth/DecodeSocialAuthController.php
+++ app/Http/Controllers/API/Auth/DecodeSocialAuthController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 use Illuminate\Validation\Rule;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -13,6 +11,7 @@ class DecodeSocialAuthController extends Controller
 {
     /**
      * Handle an incoming authentication request.
+     * @throws \Exception
      */
     public function getUser(Request $request)
     {
@@ -21,8 +20,75 @@ class DecodeSocialAuthController extends Controller
             'token' => ['required', 'string',],
         ]);
 
-        /** @var  \Laravel\Socialite\Two\User|\Laravel\Socialite\Contracts\User $google_user **/
-        $google_user = Socialite::driver($request->input('type'))->userFromToken($request->input('token'));
-        return json_decode(json_encode($google_user));
+        $type = $request->input('type');
+        $token = $request->input('token');
+        if( $type === 'google' )
+            $social_user = $this->decodeGoogleOAuthToken( $token );
+        else
+            $social_user = $this->verifyWithSocialiteAuthToken( $type, $token );
+
+//        /** @var  \Laravel\Socialite\Two\User|\Laravel\Socialite\Contracts\User $google_user **/
+//        $google_user = Socialite::driver()->userFromToken();
+//        return json_decode(json_encode($google_user));
+
+        return $social_user?? throw new \Exception("Unfortunately, the token could not be decoded!");
+    }
+
+
+    /**
+     * @param string $token
+     * @return array|null
+     */
+    private function decodeGoogleOAuthToken(string $token)
+    {
+        return $this->verifyGoogleIdToken( $token )?? $this->verifyWithSocialiteAuthToken( 'google', $token );
+    }
+
+    /**
+     * Seems this one is the only one that works with iOs Mobile
+     * @param string $id_token
+     * @return array|null
+     */
+    private function verifyGoogleIdToken(string $id_token)
+    {
+        try {
+            // https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
+            $client = new \Google_Client(['client_id' => env('GOOGLE_CLIENT_ID')]);  // Specify the CLIENT_ID of the app that accesses the backend
+            $payload = $client->verifyIdToken($id_token);
+            if ($payload) {
+                // If request specified a G Suite domain:
+                //$domain = $payload['hd'];
+                return [
+                    "social_id" => $payload['sub'],
+                    "email" => $payload['email'],
+                    "name" => $payload['name'],
+                ];
+            } else {
+                return  null;
+            }
+        }catch (\Exception){
+            return null;
+        }
+    }
+
+    /**
+     * @param string $type
+     * @param string $auth_token
+     * @return array|null
+     */
+    private function verifyWithSocialiteAuthToken(string $type, string $auth_token)
+    {
+        // this will throw an exception if failed
+        try {
+            /** @var $user \Laravel\Socialite\Two\User|\Laravel\Socialite\Contracts\User**/
+            $user = Socialite::driver($type)->userFromToken($auth_token);
+            return [
+                "social_id" => $user->getId(),
+                "email" => $user->getEmail(),
+                "name" => $user->getName(),
+            ];
+        }catch (\Exception){
+            return null;
+        }
     }
 }
